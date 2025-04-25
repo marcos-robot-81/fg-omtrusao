516b1b5ef6bfeb95a9a07d9bfa111498089b3fcf
Rayziru <marcoshenrigue81@gmail.com>
2025-04-25 05:50:34 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MenuComponent } from './component/menu/menu.component';
+import { NgOptimizedImage } from '@angular/common';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ MenuComponent, NgOptimizedImage],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'front-end';
+  img1 = 'ateroDeBrita.png';
 }
---
+++ src/app/component/menu/menu.component.ts
@@ -0,0 +1,52 @@
+import { Component } from '@angular/core';
+
+@Component({
+selector:'meno',
+template:`
+<nav>
+    <div id="div-menu">
+        <a id='menu-titulo'>Figueredo comtrução</a>
+    </div>
+    <ul>
+        <li><a>Home</a></li>
+        <li><a>Que somos</a></li>
+    </ul>
+</nav>
+`,
+styles:[`
+    nav{
+        
+        
+    }
+    ul {
+        list-style: none;
+        display: flex;
+        justify-content: space-around;
+        position: relative;
+        justify-content: center;
+        z-index: 3;
+    }
+    li {
+        background-color:rgb(255, 255, 255);
+        margin-left: 10px;
+        padding: 10px;
+    }
+    li a{
+        text-decoration: none;
+        color: black;
+        font-size: 30px;
+    }
+    #div-menu{
+        display: flex;
+        justify-content: center;}
+    #menu-titulo{
+        text-decoration: none;
+        color: black;
+        font-size: 40px
+    }
+    
+    `]
+})
+export class MenuComponent {
+
+}
