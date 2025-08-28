655fb35382d2d51122747b8d14481f86daff7a12
Marcos <marcos@MarcosDEB>
2025-08-28 11:52:46 -0300

    v1.0.2

---
+++ src/app/pajina/opção/app.apc.ts
@@ -0,0 +1,21 @@
+import { Component } from "@angular/core"
+import { CommonModule, NgOptimizedImage } from "@angular/common";
+import { RouterModule } from "@angular/router";
+
+@Component({
+    selector:"apc",
+    standalone: true,
+    imports: [CommonModule, NgOptimizedImage, RouterModule],
+    templateUrl:"./app.apc.html",
+    styleUrl:"app.apc.css"
+    
+})export class Apc{
+    materials = [
+        { name: 'Areia', image: 'midias/areia.jpg', width: 400, height: 300 },
+        { name: 'Pedra Brita', image: 'midias/pedra_brita.jpg', width: 400, height: 300 },
+        { name: 'Cimento', image: 'midias/cimento.jpg', width: 400, height: 300 },
+        { name: 'Tijolos', image: 'midias/tijolos.jpg', width: 400, height: 300 },
+        { name: 'Ferro', image: 'midias/ferro.jpg', width: 400, height: 300 },
+        { name: 'Madeira', image: 'midias/madeira.jpg', width: 400, height: 300 },
+    ];
+}
